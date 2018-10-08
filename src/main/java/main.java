e556632fbb09b332c1afe947351b22ee3487686a
Maxilyas <antoine.gomes@gmail.com>
2018-10-08 17:25:25 +0200

    Add files via upload

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+
+
+public class main {
+    public static void main(String[] args) {
+        Mastermind mastermind = new Mastermind();
+        mastermind.play();
+        //mastermind.modelMastermind();
+    }
+}
+
